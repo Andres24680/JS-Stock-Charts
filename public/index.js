43fb0d1cc207f4802f8f59e55be4e02ff856b0db
Andres24680 <tasboyandresrestrepo@aol.com>
2022-11-12 21:25:17 -0500

    setting up api key

--- public/index.js
+++ public/index.js
@@ -6,4 +6,10 @@ async function main() {
 
 }
 
+https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=30min&apikey=6a30597a905b4c8c8e75c5002b6e3b8f
+
+//api key== 6a30597a905b4c8c8e75c5002b6e3b8f
+
+let stocksResults =await stockResults.json()
+console.log(stocksResults)
 main()
