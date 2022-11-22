6731178ddaca507cad50cf693dbc9bd562ce1564
Andres24680 <tasboyandresrestrepo@aol.com>
2022-11-22 14:41:51 -0500

    should be working now

--- public/index.js
+++ public/index.js
@@ -1,16 +1,8 @@
-function getColor(stock){
-    if(stock === "GME"){
-        return 'rgba(61, 161, 61, 0.7)'
-    }
-    if(stock === "MSFT"){
-        return 'rgba(209, 4, 25, 0.7)'
-    }
-    if(stock === "DIS"){
-        return 'rgba(18, 4, 209, 0.7)'
-    }
-    if(stock === "BNTX"){
-        return 'rgba(166, 43, 158, 0.7)'
-    }
+const colors = {
+    GME: 'rgba(61, 161, 61, 0.7)',
+    MSFT: 'rgba(209, 4, 25, 0.7)',
+    DIS: 'rgba(18, 4, 209, 0.7)',
+    BNTX: 'rgba(166, 43, 158, 0.7)'
 }
 
 async function main() {
@@ -18,29 +10,26 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch("https://api.twelvedata.com/time_series?symbol=MSFT,DIS,BNTX,GME&interval=1day&apikey=d4825382148b484c91d1ba44dee435f7https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&format=JSON&apikey=d4825382148b484c91d1ba44dee435f7")
+// Was configuring the different settings that they had avaiable on the website. Sourced from https://twelvedata.com/request-builder 
+    const result = await response.json()
 
-https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=30min&apikey=6a30597a905b4c8c8e75c5002b6e3b8f
+const { GME, MSFT, DIS, BNTX } = mockData;
 
-//api key== 6a30597a905b4c8c8e75c5002b6e3b8f
+const stocks = [GME, MSFT, DIS, BNTX];
 
-const result = await response.json()
-
-    const { GME, MSFT, DIS, BNTX } = result;
-
-    const stocks = [GME, MSFT, DIS, BNTX];
-
-    stocks.forEach( stock => stock.values.reverse())
 
     // Time Chart
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
-            labels: stocks[0].values.map(value => value.datetime),
+            labels: stocks[0].values.reverse().map(value => value.datetime),
             datasets: stocks.map(stock => ({
                 label: stock.meta.symbol,
-                backgroundColor: getColor(stock.meta.symbol),
-                borderColor: getColor(stock.meta.symbol),
-                data: stock.values.map(value => parseFloat(value.high))
+                data: stock.values.reverse().map(value => parseFloat(value.high)),
+                backgroundColor: colors[stock.meta.symbol],
+                borderColor: colors[stock.meta.symbol],
+                
             }))
         }
     });
@@ -51,12 +40,12 @@ const result = await response.json()
         data: {
             labels: stocks.map(stock => stock.meta.symbol),
             datasets: [{
-                label: 'Highest',
+                label: 'Average',
                 backgroundColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
+                    colors[stock.meta.symbol]
                 )),
                 borderColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
+                    colors[stock.meta.symbol]
                 )),
                 data: stocks.map(stock => (
                     findHighest(stock.values)
@@ -73,10 +62,10 @@ const result = await response.json()
             datasets: [{
                 label: 'Average',
                 backgroundColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
+                    colors[stock.meta.symbol]
                 )),
                 borderColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
+                    colors[stock.meta.symbol]
                 )),
                 data: stocks.map(stock => (
                     calculateAverage(stock.values)
